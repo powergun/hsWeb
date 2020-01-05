72ebc7590400f4063d15118e59a2cd9dd71be463
Wei Ning <macgnw@gmail.com>
2020-01-05 18:28:45 +1100

    scotty

---
+++ scottyyy/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for scotty
+
+## Unreleased changes
---
+++ scottyyy/app/hello/Main.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Monoid (mconcat)
+import           Web.Scotty
+
+main =
+  scotty 3000 $ do
+    get "/:word" $ do
+      beam <- param "word"
+      html (mconcat ["<h1>Scotty, ", beam, " me up!</h1>"])
